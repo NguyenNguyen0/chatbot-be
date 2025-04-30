abbaf51a2701b45d3972a861df03c6a733a71c95
nguyennguyen0 <trungnguyenwork123@gmail.com>
2025-04-30 09:47:44 +0700

    Bump version to 1.1.0 and update topic classification in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chatbot-be',
-    version='1.0.0',
+    version='1.1.0',
     description='Backend API for AI-powered chatbot built with FastAPI',
     author='Nguyen Nguyen',
     url='https://github.com/NguyenNguyen0/chatbot-be',
@@ -15,7 +15,7 @@ setup(
         'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: APIs'
+        'Topic :: Software Development :: APIs :: FastAPI :: Ollama',
     ],
     python_requires='>=3.10',
 )
